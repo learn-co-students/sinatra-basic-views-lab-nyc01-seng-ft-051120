862e3472a3ae16160b1eb01dd596b107ff7d4b62
callas4ever <thetseringnorbu@gmail.com>
2020-06-02 14:55:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-
+get '/' do
+    "<h1>Buster's Best Bowling Alley</h1>"
+    erb :index
+end
+get '/' do
+    erb :info
+end
 end
